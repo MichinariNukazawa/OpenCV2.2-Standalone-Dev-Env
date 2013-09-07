4986825b40bbaac9a2fa34da50ed87ef9d2c956f
MichinariNukazawa <michinari.nukazawa@gmail.com>
2013-09-07 18:07:17 +0900

    first commit

---
+++ !cons.bat
@@ -0,0 +1,23 @@
+
+:: GCCがヘッダファイルを探索するための環境変数設定
+:: ＆ヘッダファイルのインクルードパス
+set C_INCLUDE_PATH=opencv\build\include\opencv;%C_INCLUDE_PATH%
+:: opencv2へのパス追加
+set C_INCLUDE_PATH=opencv\build\include;%C_INCLUDE_PATH%
+:: C++対応(未確認)
+set CPULS_INCLUDE_PATH=%C_INCLUDE_PATH%;%CPULS_INCLUDE_PATH%
+
+
+::GCCが静的ライブラリを探索するための環境変数＆パス指定
+set LIBRARY_PATH=opencv\build\x86\mingw\lib;%LIBRARY_PATH%
+
+::GCCが動的ライブラリを探索するための環境変数＆パス指定
+set PATH=opencv\build\x86\mingw\bin;%PATH%
+
+
+:: "エイリアスの追加(make runできるようになる。)"
+doskey make = mingw32-make $*
+doskey rm = del $*
+
+
+cmd.exe
---
+++ README.md
@@ -0,0 +1,10 @@
+OpenCV2.4の開発環境です。
+USBなどで手軽に持ち運べるのが特徴です。
+また、開発マシンに既に別のOpenCVライブラリが入っていても、互いに影響がありません。
+
+別途、MinGWとOpenCV2.4(のWindows版ライブラリ)を入手する必要があります。
+詳しい使い方や説明はブログ記事(http://blog.michinari-nukazawa.com)をご参照ください。
+
+
+文字コードはWindowsで扱いやすいように(多分)Shift-JISになっています。
+Linux環境では、単にWindows対応可能なプロジェクトディレクトリです。(makefileの対応が不完全)
