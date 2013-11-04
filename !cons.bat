ad5666a64ac65d0ae3ee9d4ff358887cf632f98a
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2013-11-04 10:44:42 +0900

    first commit Opencv2.2 build environment. (from OpenCV2.4.6 build environment.)

--- !cons.bat
+++ !cons.bat
@@ -1,22 +1,27 @@
+::
+::このファイルと同じディレクトリ内にOpenCV2.2およびMinGWが存在しない場合、
+::c:\直下(デフォルトインストール先)にあるファイルを使用する。
+::
+
 
 :: GCCがヘッダファイルを探索するための環境変数設定
 :: &ヘッダファイルのインクルードパス
-set C_INCLUDE_PATH=opencv\build\include\opencv;%C_INCLUDE_PATH%
+set C_INCLUDE_PATH=OpenCV2.2\include\opencv;c:\OpenCV2.2\include\opencv;%C_INCLUDE_PATH%
 :: opencv2へのパス追加
-set C_INCLUDE_PATH=opencv\build\include;%C_INCLUDE_PATH%
+set C_INCLUDE_PATH=OpenCV2.2\include;c:\OpenCV2.2\include;%C_INCLUDE_PATH%
 :: C++対応(未確認)
 set CPULS_INCLUDE_PATH=%C_INCLUDE_PATH%;%CPULS_INCLUDE_PATH%
 
 
 ::GCCが静的ライブラリを探索するための環境変数&パス指定
-set LIBRARY_PATH=opencv\build\x86\mingw\lib;%LIBRARY_PATH%
+set LIBRARY_PATH=OpenCV2.2\lib;c:\OpenCV2.2\lib;%LIBRARY_PATH%
 
 ::GCCが動的ライブラリを探索するための環境変数&パス指定
-set PATH=opencv\build\x86\mingw\bin;%PATH%
+set PATH=OpenCV2.2\bin;c:\OpenCV2.2\bin;%PATH%
 
 
 ::MinGW(gcc, make)を使用するための環境変数&パス指定
-set PATH=MinGW\bin;%PATH%
+set PATH=MinGW\bin;c:\MinGW\bin;%PATH%
 
 
 :: "エイリアスの追加(make runできるようになる。)"
--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-OpenCV 2.4.6の開発環境です。
+OpenCV 2.2の開発環境です。
 USBなどで手軽に持ち運べるのが特徴です。
 また、開発マシンに既に別のOpenCVライブラリが入っていても、互いに影響がありません。
 
-別途、MinGWとOpenCV 2.4.6(のWindows版ライブラリ)を入手する必要があります。
+別途、MinGWとOpenCV 2.2(のWindows版ライブラリ)を入手する必要があります。
 詳しい使い方や説明はブログ記事(http://blog.michinari-nukazawa.com/2013/09/opencv24-and-mingw-gcc-compile.html )をご参照ください。
 
-OpenCVライブラリ 2.4.6以外のバージョンを使用する場合、makefileの対応箇所(VER=246)を書き換えてください。
+OpenCVライブラリ 2.2以外のバージョンを使用する場合、各ファイルの対応箇所を書き換えてください。
+(makefileのVER=220、!cons.batのパスほか。)
 
 文字コードはWindowsで扱いやすいように(多分)Shift-JISになっています。
 
